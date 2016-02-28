6fde75afe18e134c59120229ea37d639b0e00031
Joshua Loyal <joshua.d.loyal@gmail.com>
2016-02-28 10:20:23 -0500

    fixed whl package name

--- setup_tensorflow.sh
+++ setup_tensorflow.sh
@@ -22,4 +22,4 @@ cd tensorflow
 # build python package
 bazel build -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
-pip install /tmp/tensorflow_pkg/tensorflow-0.7.1-py2-none-linux_x86_64.whl
+pip install /tmp/tensorflow_pkg/tensorflow-0.7.1-py2-none-any.whl
